8dc1ec542521eedc62b45404712669ec4f10715a
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-01-28 20:00:38 -0500

    added xbox drive command

---
+++ src/main/java/frc/robot/commands/DriveXbox.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drive;
+
+public class DriveXbox extends CommandBase {
+    private Drive mDriveTrain;
+    private XboxController mController;
+    private double pastInput;
+    private boolean accelerate;
+
+    /**
+     * Creates a new DriveWithJoysticks.
+     */
+    public DriveXbox(XboxController xboxController) {
+        mDriveTrain = Drive.getInstance();
+        mController = xboxController;
+        addRequirements(mDriveTrain);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        accelerate = false;
+        pastInput = 0;
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        if (accelerate && Math.abs(mDriveTrain.getLeftVelocity()) > 0) {
+            accelerate = true;
+        }
+        else {
+            accelerate = mController.getLeftY() - pastInput < 0;
+        }
+        pastInput = mController.getLeftY();
+        if (accelerate) {
+            mDriveTrain.drive(mDriveTrain.getRateLimit().calculate(mController.getLeftY()), mController.getRightX() * 0.75);
+        }
+        else {
+            mDriveTrain.drive(mController.getLeftY(), mController.getRightX() * 0.75);
+        }
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
