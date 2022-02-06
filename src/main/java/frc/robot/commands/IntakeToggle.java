77c4b65b1039ffb1bd62076e93ee5663d2883d93
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-02-05 21:17:02 -0500

    made constants import static

--- src/main/java/frc/robot/commands/IntakeToggle.java
+++ src/main/java/frc/robot/commands/IntakeToggle.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Intake;
 
-
 public class IntakeToggle extends CommandBase {
     private final Intake intake = Intake.getInstance();
 
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,17 +1,16 @@
 package frc.robot.subsystems;
 
-
 import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.util.LazySparkMax;
+import static frc.robot.Constants.*;
 
 public class Intake extends SubsystemBase {
-    private final DoubleSolenoid solenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.INTAKE_PNEUMATIC_FORWARD, Constants.INTAKE_PNEUMATIC_REVERSE);
-    private final LazySparkMax intake = new LazySparkMax(Constants.INTAKE_CAN_MAIN, CANSparkMax.IdleMode.kBrake);
+    private final DoubleSolenoid solenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, INTAKE_PNEUMATIC_FORWARD, INTAKE_PNEUMATIC_REVERSE);
+    private final LazySparkMax intake = new LazySparkMax(INTAKE_CAN_MAIN, CANSparkMax.IdleMode.kBrake);
 
     private final static Intake INSTANCE = new Intake();
     public static Intake getInstance() {
@@ -36,7 +35,7 @@ public class Intake extends SubsystemBase {
     }
 
     public void intakeOn() {
-        intake.set(Constants.INTAKE_MAIN_SPEED);
+        intake.set(INTAKE_MAIN_SPEED);
     }
 
     public void intakeOff() {
@@ -44,7 +43,7 @@ public class Intake extends SubsystemBase {
     }
 
     public void reverseIntake() {
-        intake.set(-Constants.INTAKE_MAIN_SPEED);
+        intake.set(-INTAKE_MAIN_SPEED);
     }
 
     public DoubleSolenoid.Value getState() {
