1c1eb291c088a3df17f244bb307c24d3dd9bc9f5
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-02-10 17:07:06 -0500

    cleaned up limelight

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -9,64 +8,10 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.Limelight.*;
 
 public class Limelight extends SubsystemBase {
-    private static Limelight INSTANCE = new Limelight();
-
-    public static Limelight getInstance() {
-        return INSTANCE;
-    }
-
-    private Limelight() {
-        setLED(LED.OFF);
-        setCamMode(Camera.DRIVER);
-        LimelightManager limelightManager = new LimelightManager();
-    }
-
-    private static class LimelightManager {
-        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-
-        //NetworkTable key names can be found at https://docs.limelightvision.io/en/latest/networktables_api.html
-        //Outputs from Limelight:
-        NetworkTableEntry tv = table.getEntry("tv");
-        NetworkTableEntry tx = table.getEntry("tx");
-        NetworkTableEntry ty = table.getEntry("ty");
-        NetworkTableEntry ta = table.getEntry("ta");
-        NetworkTableEntry ts = table.getEntry("ts");
-        NetworkTableEntry tl = table.getEntry("tl");
-        //NetworkTableEntry tshort = table.getEntry("tshort");
-        //NetworkTableEntry tlong = table.getEntry("tlong");
-        NetworkTableEntry thor = table.getEntry("thor");
-        NetworkTableEntry tvert = table.getEntry("tvert");
-        NetworkTableEntry getpipe = table.getEntry("getpipe");
-        //NetworkTableEntry camtran = table.getEntry("camtran"); //6DOF 3D position solution (translation + YPR)
-
-        //Inputs to Limelight:
-        NetworkTableEntry ledMode = table.getEntry("ledMode");
-        NetworkTableEntry camMode = table.getEntry("camMode");
-        NetworkTableEntry pipeline = table.getEntry("pipeline");
-        //NetworkTableEntry stream = table.getEntry("stream");
-        //NetworkTableEntry snapshot = table.getEntry("snapshot");
-
-    }
-    LimelightManager limelightManager = new LimelightManager();
-
-    public boolean hasTarget() {
-        return limelightManager.tv.getDouble(0.0) == 1.0;
-    }
-
-    public double getHorizontal() {
-        return limelightManager.tx.getDouble(0.0);
-    }
-
-    public double getVertical() {
-        return limelightManager.ty.getDouble(0.0);
-    }
-
-    public double getArea() {
-        return limelightManager.ta.getDouble(0.0);
-    }
+    private final NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
 
     public enum Pipelines {
-        ; //the cursed lone semicolon
+        ;
 
         private int num;
         Pipelines(int num) {
@@ -77,11 +22,6 @@ public class Limelight extends SubsystemBase {
             return num;
         }
     }
-
-    public void setPipeline(Pipelines pipeline) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(pipeline.getNum());
-    }
-
 
     public enum LED {
         PIPELINE(0), OFF(1), BLINK(2), ON(3);
@@ -97,17 +37,12 @@ public class Limelight extends SubsystemBase {
         }
     }
 
-    public void setLED(LED mode) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(mode.getMode());
-    }
-
-
-    public enum Camera {
-        VISION(0), DRIVER(1); //michael please fix your atrocious naming schemes thanks
+    public enum CameraMode {
+        VISION_PROCESSING(0), DRIVER_CAMERA(1);
 
         private int num;
 
-        Camera(int num) {
+        CameraMode(int num) {
             this.num = num;
         }
 
@@ -116,28 +51,61 @@ public class Limelight extends SubsystemBase {
         }
     }
 
-    public void setCamMode(Camera mode) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(mode.getNum());
+    private static final Limelight INSTANCE = new Limelight();
+    public static Limelight getInstance() {
+        return INSTANCE;
     }
 
-
-    public void enableLimelight() {
-        setCamMode(Camera.VISION);
-        setLED(LED.ON);
-    }
-
-    public void disableLimelight() {
-        setCamMode(Camera.DRIVER);
+    private Limelight() {
         setLED(LED.OFF);
-    }
-
-    public double getDistance() {
-        return (LIMELIGHT_TARGET_HEIGHT - LIMELIGHT_MOUNTING_HEIGHT) / Math.tan(LIMELIGHT_ANGLE + getVertical());
+        setCamMode(CameraMode.DRIVER_CAMERA);
     }
 
     public void periodic() {
         SmartDashboard.putNumber("Limelight Horizontal", getHorizontal());
         SmartDashboard.putNumber("Limelight Vertical", getVertical());
         SmartDashboard.putNumber("Limelight Area", getArea());
+    }
+
+    public boolean hasTarget() {
+        return limelight.getEntry("tv").getDouble(0.0) == 1.0;
+    }
+
+    public double getHorizontal() {
+        return limelight.getEntry("tx").getDouble(0.0);
+    }
+
+    public double getVertical() {
+        return limelight.getEntry("ty").getDouble(0.0);
+    }
+
+    public double getArea() {
+        return limelight.getEntry("ta").getDouble(0.0);
+    }
+
+    public void setPipeline(Pipelines pipeline) {
+        limelight.getEntry("pipeline").setNumber(pipeline.getNum());
+    }
+
+    public void setLED(LED mode) {
+        limelight.getEntry("ledMode").setNumber(mode.getMode());
+    }
+
+    public void setCamMode(CameraMode mode) {
+        limelight.getEntry("camMode").setNumber(mode.getNum());
+    }
+
+    public void enable() {
+        setCamMode(CameraMode.VISION_PROCESSING);
+        setLED(LED.ON);
+    }
+
+    public void disable() {
+        setCamMode(CameraMode.DRIVER_CAMERA);
+        setLED(LED.OFF);
+    }
+
+    public double getDistance() {
+        return (LIMELIGHT_TARGET_HEIGHT - LIMELIGHT_MOUNTING_HEIGHT) / Math.tan(LIMELIGHT_ANGLE + getVertical());
     }
 }
