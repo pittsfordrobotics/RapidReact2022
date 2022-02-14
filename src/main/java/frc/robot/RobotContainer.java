c48f7f7856eaea4571a1b2ef75d1b8579ddeab13
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-02-13 22:46:22 -0500

    stored controllers statically inside of BetterXboxController

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,15 +9,14 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.DriveXbox;
 import frc.robot.subsystems.Drive;
 import frc.robot.util.controller.BetterXboxController;
+import frc.robot.util.controller.BetterXboxController.Humans;
 import frc.robot.util.controller.BetterXboxController.Hand;
 
 public class RobotContainer {
-  //  Subsystems
   private final Drive drive = Drive.getInstance();
 
-  //  Controllers
-  public static final BetterXboxController driverController = new BetterXboxController(0, Hand.RIGHT);
-  public static final BetterXboxController operatorController = new BetterXboxController(1);
+  private final BetterXboxController driverController = new BetterXboxController(0, Hand.RIGHT, Humans.DRIVER);
+  private final BetterXboxController operatorController = new BetterXboxController(1, Humans.OPERATOR);
 
   public RobotContainer() {
     configureButtonBindings();
