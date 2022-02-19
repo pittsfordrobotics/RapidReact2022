81cb7df4c077aef437f62a1f58f1ea826e8ca3ff
asclol <caoboystx0609@gmail.com>
2022-02-19 15:51:06 -0500

    hi

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,6 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+// SHUT UP NOBODY CARES
+
 package frc.robot;
 
 import com.ctre.phoenix.sensors.Pigeon2Configuration;
