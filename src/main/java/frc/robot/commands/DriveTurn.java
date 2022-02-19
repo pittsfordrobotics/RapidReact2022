3bc13531d55f60410de16c8f04ae39af5aae532f
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-02-19 15:14:06 -0500

    added PID controller

--- src/main/java/frc/robot/commands/DriveTurn.java
+++ src/main/java/frc/robot/commands/DriveTurn.java
@@ -4,42 +4,48 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drive;
 
 public class DriveTurn extends CommandBase {
   private final double angle;
   private final Drive drive = Drive.getInstance();
+  private final PIDController pidController = new PIDController(1,0,0);
   private double endingAngle;
-  /** Creates a new DriveTurn. */
+  private double throttle;
+
   public DriveTurn(double angle) {
     this.angle = angle;
     addRequirements(this.drive);
-    
-
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    this.endingAngle = drive.getAngle() + angle;
+    pidController.setSetpoint();
+    throttle = drive.getThrottle();
+    endingAngle = drive.getAngle() + angle;
+    drive.setThrottle(0.6);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(angle > 0) {
-    drive.driveArcade(0, 0.25);
+    if(angle < 0) {
+      drive.driveArcade(0, 1);
     }
     else {
-      drive.driveArcade(0, -0.25);
+      drive.driveArcade(0, -1);
     }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drive.setThrottle(throttle);
+  }
 
   // Returns true when the command should end.
   @Override
