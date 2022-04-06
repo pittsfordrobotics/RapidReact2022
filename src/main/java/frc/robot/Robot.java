e334d0f88a4647576a67d37119d9d1fe782c706b
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-04-06 19:59:27 -0400

    added better logger

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -12,7 +13,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.CG_ClimberCalibrate;
 import frc.robot.subsystems.*;
-import frc.robot.util.TestLogger;
+import frc.robot.util.BetterLogger;
 import frc.robot.util.controller.BetterXboxController;
 
 public class Robot extends TimedRobot {
@@ -41,15 +42,17 @@ public class Robot extends TimedRobot {
     intake.retract();
     limelight.disable();
     indexer.disable();
-    TestLogger.enable();
+//    TestLogger.enable();
+    BetterLogger.enable();
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    if (DriverStation.isDSAttached()) BetterLogger.createFile();
     new BetterXboxController(0, BetterXboxController.Hand.LEFT, BetterXboxController.Humans.DRIVER);
     new BetterXboxController(1, BetterXboxController.Humans.OPERATOR);
-    TestLogger.logData();
+//    TestLogger.logData();
   }
 
   @Override
--- src/main/java/frc/robot/util/BetterLogger.java
+++ src/main/java/frc/robot/util/BetterLogger.java
@@ -1,45 +1,110 @@
 package frc.robot.util;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Timer;
+import org.json.simple.JSONObject;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class BetterLogger {
-    private final String logFolder = "/media/sda2/";
-    private final String logTitle = "'TestLogger'_MM-dd-yyyy_HH:mm:ss'.csv'";
-    private final String basicLogValues = "Timestamp,Enabled,X,Y,Rotation,Throttle,Shooter Armed,Indexer Balls,Target Detected";
-    private final String detailedLogValues = "Target Distance,Target Horizontal,Target Vertical,Shooter Speed,Shooter Set Speed,Indexer State,Ball0 Color,Ball0 Location,Ball1 Color,Ball1 Location,Compressor State,Intake Extended,Climber Encoder,Climber Forward Limit,Climber Reverse Limit,Pigeon Angle,Battery Voltage,CAN Bus Utilization";
-    private final String xboxLogValues = "Connected,Left X,Left Y,Right X,Right Y,Right Stick Button,Left Stick Button,A,B,X,Y,Start,Back,Right Bumper,Left Bumper,Right Trigger,Left Trigger,DPad";;
-    private final Timer logTimer = new Timer();
-    private final double logRate = 0.1;
-    private boolean loggerEnabled = false;
+    private static final String logFolder = "/media/sda2/";
+    private static final String logTitle = "'BetterLogger'_MM-dd-yyyy_HH:mm:ss'.json'";
+    private static String logPath;
 
-    private FileWriter logger;
+    private static final String basicLogValues = "Timestamp,Enabled,X,Y,Rotation,Throttle,Shooter Armed,Indexer Balls,Target Detected";
+    private static final Timer logTimer = new Timer();
+    private static final double logRate = 0.1;
 
-    private BetterLogger() {
-        String logPath = logFolder + new SimpleDateFormat(logTitle).format(new Date());
-        try {
-            logTimer.reset();
-            logTimer.start();
-            System.out.println(new File(logPath).createNewFile() ? "Successfully created file" : "Failed to create file");
-            logger = new FileWriter(logPath);
-            logger.write(new SimpleDateFormat("MM-dd-yyyy_HH:mm:ss").format(new Date()) + "\n");
-            logger.write(DriverStation.getAlliance().name() + "\n");
-            logger.write(basicLogValues + "," + detailedLogValues + ",Driver," + xboxLogValues + ",Operator," + xboxLogValues + "\n");
-            logger.flush();
-            System.out.println("Successfully opened log file '" + logPath + "'");
-        } catch (IOException e) {
-            DriverStation.reportWarning("Failed to open log file '" + logPath + "'", false);
+    private static boolean loggerEnabled = false;
+    private static boolean fileMade = false;
+
+    private static FileWriter fileWriter;
+    private static final JSONObject jsonWriter = new JSONObject();
+    private static final JSONObject jsonArray = new JSONObject();
+
+    private static final HashMap<String, Supplier> supplierHelper = new HashMap<>();
+
+    private static final Alert fileError = new Alert("Failed to open log file. Logging will NOT work!", Alert.AlertType.WARNING);
+    private static final Alert loggingError = new Alert("There was an error while logging data! Logging might not work!", Alert.AlertType.WARNING);
+
+    public static void createFile() {
+        if (!fileMade) {
+            logPath = logFolder + new SimpleDateFormat(logTitle).format(new Date());
+            try {
+                logTimer.reset();
+                logTimer.start();
+                fileWriter = new FileWriter(logPath);
+                JSONObject jsonInner = new JSONObject();
+                jsonInner.put("date", new SimpleDateFormat("MM-dd-yyyy_HH:mm:ss").format(new Date()));
+                jsonInner.put("match", DriverStation.getMatchNumber());
+                jsonInner.put("alliance", DriverStation.getAlliance().name());
+                jsonWriter.put("setup", jsonInner);
+                fileWriter.write(jsonWriter.toJSONString());
+                fileWriter.flush();
+                fileWriter.close();
+                fileMade = true;
+                fileError.set(false);
+                System.out.println("Successfully opened log file '" + logPath + "'");
+            } catch (IOException e) {
+                fileError.set(true);
+            }
         }
     }
 
-    private static final BetterLogger INSTANCE = new BetterLogger();
-    public static BetterLogger getInstance() {
-        return INSTANCE;
+    public static void enable() {
+        loggerEnabled = true;
     }
+    public static void disable() {
+        loggerEnabled = false;
+    }
+
+    public static void logPeriodic() {
+        if (loggerEnabled) {
+            try {
+                fileWriter = new FileWriter(logPath);
+                JSONObject jsonInner = new JSONObject();
+                jsonInner.put("enabled", DriverStation.isEnabled() ? 1 : 0);
+                jsonInner.put("battery", RobotController.getBatteryVoltage());
+                jsonInner.put("brownedOut", RobotController.isBrownedOut() ? 1 : 0);
+                jsonInner.put("CANUtil", RobotController.getCANStatus().percentBusUtilization);
+                for (Map.Entry<String, Supplier> data : supplierHelper.entrySet()) {
+                    try {
+                        jsonInner.put(data.getKey(), String.format("%.4f", data.getValue().get()));
+                    } catch (Exception e) {
+                        jsonInner.put(data.getKey(), data.getValue().get());
+                    }
+                }
+                jsonWriter.put(String.format("%.4f", Timer.getFPGATimestamp()), jsonArray);
+                fileWriter.write(jsonWriter.toString());
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (Exception e) {
+                loggingError.set(true);
+            }
+        }
+    }
+
+    public static void logDrive(String key, Supplier<Double> valueSupplier) {
+        supplierHelper.put(key, valueSupplier);
+    }
+
+    public static void logString(String key, Supplier<String> valueSupplier) {
+        supplierHelper.put(key, valueSupplier);
+    }
+
+    public static void logNumber(String key, Supplier<Double> valueSupplier) {
+        supplierHelper.put(key, valueSupplier);
+    }
+
+    public static void logBoolean(String key, Supplier<Boolean> valueSupplier) {
+        supplierHelper.put(key, valueSupplier);
+    }
+
 }
