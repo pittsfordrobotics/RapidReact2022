14a7f860a37a622061153d15852c05bf7c8cdaff
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-04-06 23:13:37 -0400

    added a bunch of defaults

--- src/main/java/frc/robot/util/BetterLogger.java
+++ src/main/java/frc/robot/util/BetterLogger.java
@@ -32,7 +32,7 @@ public class BetterLogger {
     private static final HashMap<String, Supplier> supplierHelper = new HashMap<>();
 
     private static final Alert fileError = new Alert("Failed to open log file. Logging will NOT work!", Alert.AlertType.WARNING);
-    private static final Alert loggingError = new Alert("There was an error while logging data! Logging might not work!", Alert.AlertType.WARNING);
+    private static final Alert loggingError = new Alert("There was an error while writing logging data! Logging will NOT work!", Alert.AlertType.WARNING);
 
     public static void createFile() {
         if (!fileMade) {
@@ -61,6 +61,7 @@ public class BetterLogger {
     public static void enable() {
         loggerEnabled = true;
     }
+
     public static void disable() {
         loggerEnabled = false;
     }
@@ -70,9 +71,9 @@ public class BetterLogger {
             try {
                 fileWriter = new FileWriter(logPath);
                 JSONObject jsonInner = new JSONObject();
-                jsonInner.put("enabled", DriverStation.isEnabled() ? 1 : 0);
-                jsonInner.put("battery", RobotController.getBatteryVoltage());
-                jsonInner.put("brownedOut", RobotController.isBrownedOut() ? 1 : 0);
+                jsonInner.put("Enabled", DriverStation.isEnabled() ? 1 : 0);
+                jsonInner.put("Battery", RobotController.getBatteryVoltage());
+                jsonInner.put("Browned Out", RobotController.isBrownedOut() ? 1 : 0);
                 jsonInner.put("CANUtil", RobotController.getCANStatus().percentBusUtilization);
                 for (Map.Entry<String, Supplier> data : supplierHelper.entrySet()) {
                     try {
@@ -91,10 +92,6 @@ public class BetterLogger {
         }
     }
 
-    public static void logDrive(String key, Supplier<Double> valueSupplier) {
-        supplierHelper.put(key, valueSupplier);
-    }
-
     public static void logString(String key, Supplier<String> valueSupplier) {
         supplierHelper.put(key, valueSupplier);
     }
@@ -105,6 +102,28 @@ public class BetterLogger {
 
     public static void logBoolean(String key, Supplier<Boolean> valueSupplier) {
         supplierHelper.put(key, valueSupplier);
+    }
+
+    public static void logWestCoastDrive(Supplier<Double> x, Supplier<Double> y, Supplier<Double> rot, Supplier<Double> throttle) {
+        supplierHelper.put("Drive/X", x);
+        supplierHelper.put("Drive/Y", y);
+        supplierHelper.put("Drive/Rotation", rot);
+        supplierHelper.put("Drive/Throttle", throttle);
+    }
+
+    public static void logFlywheel(Supplier<Boolean> armed, Supplier<Double> setSpeed, Supplier<Double> realSpeed) {
+        supplierHelper.put("Shooter/Armed", armed);
+        supplierHelper.put("Shooter/Set Speed", setSpeed);
+        supplierHelper.put("Shooter/Real Speed", realSpeed);
+    }
+
+    public static void logLimelight(Supplier<Double> hasTarget, Supplier<Double> tx, Supplier<Double> ty, Supplier<Double> distance, Supplier<Integer> LEDStatus, Supplier<Integer> pipeline, Supplier<Integer> cameraMode) {
+        supplierHelper.put("Limelight/Has Target", hasTarget);
+        supplierHelper.put("Limelight/Horizontal Angle", tx);
+        supplierHelper.put("Limelight/Vertical Angle", ty);
+        supplierHelper.put("Limelight/LED Status", LEDStatus);
+        supplierHelper.put("Limelight/Pipeline", pipeline);
+        supplierHelper.put("Limelight/Camera Mode", cameraMode);
     }
 
 }
