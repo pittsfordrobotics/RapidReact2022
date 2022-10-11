cd561018874e599b8eaeac140d4fc930b7267966
Michael Yu <64374317+itsmichaelyu@users.noreply.github.com>
2022-10-11 15:59:18 -0400

    removed snap check

--- src/main/java/frc/robot/commands/CG_LimeShot.java
+++ src/main/java/frc/robot/commands/CG_LimeShot.java
@@ -7,7 +7,7 @@ import frc.robot.Constants;
 public class CG_LimeShot extends SequentialCommandGroup {
     public CG_LimeShot() {
         super(
-                new DriveCheckSnapped(),
+//                new DriveCheckSnapped(),
                 new ShooterHoodLime(),
                 new ShooterHoodPrimed(),
                 new IndexerShoot(),
